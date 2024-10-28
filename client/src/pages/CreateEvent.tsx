a4d4c09c5523162f3fdc4cb3fcbf2654cabdef57
marih <marih@itu.dk>
2024-10-28 12:52:09 +0100

    Update CreateEvent.tsx

--- client/src/pages/CreateEvent.tsx
+++ client/src/pages/CreateEvent.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import Layout from "./Layout";
+import //styling.css
+
 function CreateEvent() {
-  return <>Hello world!</>;
+return (
+ <div className="container">
+  <div className="header">
+    <h2>Create Event</h2>
+ </div>
 }
 
 export default CreateEvent;
