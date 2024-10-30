e4b4dbb179f7eb31568aa2dfb14a6491b8289148
Freja Kjærsig Sunesen <kjaersig@live.dk>
2024-10-30 16:27:04 +0100

    ProfileForms oprettet

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,15 @@
+import { ProfileForm } from "../components/ProfileForm";
+import { DogProfileForm } from "../components/DogProfileForm";
+
 function Profile() {
-  return <>Hello world!</>;
+  return (
+    <div>
+      <h1>Your profile</h1>
+      <ProfileForm />
+      <h1>Your dogs</h1>
+      <DogProfileForm />
+    </div>
+  );
 }
 
 export default Profile;
