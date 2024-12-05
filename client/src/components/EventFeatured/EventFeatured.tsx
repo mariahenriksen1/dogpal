412ca685ef8c6e3837596089bdd02411cd960498
niklasbentzen <bent@itu.dk>
2024-12-05 09:41:23 +0100

    Images hold size without content

--- client/src/components/EventFeatured/EventFeatured.tsx
+++ client/src/components/EventFeatured/EventFeatured.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./EventFeatured.module.css";
 import Information from "../Information/Information.tsx";
-import IEvent from "../../interfaces.ts";
+import { IEvent } from "../../interfaces.ts";
 import { FaEuroSign, FaUser } from "react-icons/fa";
 import { FaLocationPin } from "react-icons/fa6";
 
@@ -11,36 +11,34 @@ interface EventFeaturedProps {
 
 function EventFeatured({ event }: EventFeaturedProps) {
   return (
-    <Link to="/test">
-      <div className={styles.featured}>
-        <div className={styles.graphic}>
-          <img src={event.image} alt="Event photo" />
-          <div className={styles.overlay}>
-            <h1 className="color-white">{event.name}</h1>
-            <div className={styles.information}>
-              <Information
-                icon={<FaLocationPin color={"white"} />}
-                text={event.location}
-                color="white"
-              />
-              <Information text={event.date} color="white" />
-            </div>
-          </div>
-        </div>
-        <div
-          style={{ maxWidth: "400px" }}
-          className="flex-column gap-10 align-baseline"
-        >
-          <div className="featured-information flex-row gap-20">
+    <Link to="/test" className={styles.featured}>
+      <div className={styles.graphic}>
+        <img src={event.image} alt="Event photo" />
+        <div className={styles.overlay}>
+          <h1 className="color-white">{event.name}</h1>
+          <div className={styles.information}>
             <Information
-              icon={<FaEuroSign color={"black"} />}
-              text={String(event.price)}
-              color="black"
+              icon={<FaLocationPin color={"white"} />}
+              text={event.location}
+              color="white"
             />
-            <Information icon={<FaUser />} text={event.creator} color="black" />
+            <Information text={event.date} color="white" />
           </div>
-          <p>{event.description}</p>
         </div>
+      </div>
+      <div
+        style={{ maxWidth: "400px" }}
+        className="flex-column gap-10 align-baseline"
+      >
+        <div className="featured-information flex-row gap-20">
+          <Information
+            icon={<FaEuroSign color={"black"} />}
+            text={String(event.price)}
+            color="black"
+          />
+          <Information icon={<FaUser />} text={event.creator} color="black" />
+        </div>
+        <p>{event.description}</p>
       </div>
     </Link>
   );
