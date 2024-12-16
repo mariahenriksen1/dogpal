215a5eec11e3256904a50408f7510c5f03bb6fec
Freja Kjærsig Sunesen <kjaersig@live.dk>
2024-12-16 14:11:49 +0100

    PreviewImage component updated for enhanced usability and flexibility

--- client/src/components/PreviewImage/PreviewImage.tsx
+++ client/src/components/PreviewImage/PreviewImage.tsx
@@ -4,10 +4,25 @@ import "./PreviewImage.modules.css"; // Import the CSS file
 interface PreviewImageProps {
   src: string;
   alt: string;
+  pictureSize?: string; // Add pictureSize prop
+  onError?: () => void;
 }
 
-const PreviewImage: React.FC<PreviewImageProps> = ({ src, alt }) => {
-  return <img src={src} alt={alt} className="profile-picture-preview" />;
+const PreviewImage: React.FC<PreviewImageProps> = ({
+  src,
+  alt,
+  pictureSize,
+  onError,
+}) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={"picture-preview"}
+      onError={onError}
+      style={{ width: pictureSize, height: pictureSize }} // Apply pictureSize dynamically
+    />
+  );
 };
 
 export default PreviewImage;
